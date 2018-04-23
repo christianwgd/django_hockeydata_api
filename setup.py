98e68aad829a5443340ceb6204f230983ad217f7
Christian Wiegand <cwiegand@wgdnet.de>
2018-04-23 13:03:34 +0200

    example neu strukturiert

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup
 
 setup(name='django_hockeydata_api',
-      version='0.1',
+      version='0.0.1',
       description='A Django package for simple use of Hockeydata Javascript API',
       url='https://git.wgdnet.de/cwiegand/django_hockeydata_api.git',
       author='Christian Wiegand',
       author_email='christianwgd@gmail.com',
       license='BSD',
-      packages=['hockeydata_api'],
+      long_description=open('README.rst').read(),
+      packages=['hockeydata_api', ],
+      include_package_data=True,
       install_requires=[
-          "django >= 1.11",
       ],
-      zip_safe=False)
+      zip_safe=False,
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          'Framework :: Django',
+          "License :: OSI Approved :: BSD License",
+      ],)
